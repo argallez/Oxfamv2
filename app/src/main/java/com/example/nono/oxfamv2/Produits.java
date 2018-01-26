84cd91b79e51ad8689ece10962ee6577f9993e62
Arnaud Galle <arnaudgallez@outlook.com>
2018-01-26 23:59:51 +0100

    Produits organise

--- app/src/main/java/com/example/nono/oxfamv2/Produits.java
+++ app/src/main/java/com/example/nono/oxfamv2/Produits.java
@@ -13,58 +13,9 @@ import static com.example.nono.oxfamv2.MainActivity.tabQuant;
  */
 
 public class Produits{
-    //Getters & Setters
-    public String getNomProduits() {
-        return nomProduits;
-    }
-
-
-
-    public int getNbreProduitsPlus() {
-        return nbreProduitsPlus;
-    }
-
-    public float getPrixProduits() {
-        return prixProduits;
-    }
-
-    public int getTab() {
-        return tab;
-    }
-
-    public int getId() {
-        return this.id;
-    }
-
-    public int getStock() {
-        return this.stock;
-    }
-
-    public void setNomProduits(String nomProduits) {
-        this.nomProduits = nomProduits;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
-
-    public void setNbreProduitsPlus(int nbreProduitsPlus) {
-        this.nbreProduitsPlus = nbreProduitsPlus;
-    }
-
-    public void setPrixProduits(float prixProduits) {
-        this.prixProduits = prixProduits;
-    }
-
-    public void setTab(int tab) {
-        this.tab = tab;
-    }
-
-
-
     //Variables d'instance
     private String nomProduits;
-
+    private String instance_name;
     private int nbreProduitsPlus;
     private float prixProduits;
     private int tab;
@@ -75,15 +26,66 @@ public class Produits{
 
 
     //Constructeur
-    public Produits (String pNom, float pPrix, int ptab){
+    public Produits (String pNom, String instance_name, float pPrix, int ptab){
         this.nomProduits= pNom;
         this.prixProduits = pPrix;
         this.nbreProduitsPlus = 0;
         this.tab=ptab;
+        this.instance_name = instance_name;
         this.id = ID;
         ID++;
 
-        }
+    }
+    //Getters & Setters
+    public String getNomProduits() {
+        return nomProduits;
+    }
+    public void setNomProduits(String nomProduits) {
+        this.nomProduits = nomProduits;
+    }
+
+    public int getNbreProduitsPlus() {
+        return nbreProduitsPlus;
+    }
+    public void setNbreProduitsPlus(int nbreProduitsPlus) {
+        this.nbreProduitsPlus = nbreProduitsPlus;
+    }
+
+    public float getPrixProduits() {
+        return prixProduits;
+    }
+    public void setPrixProduits(float prixProduits) {
+        this.prixProduits = prixProduits;
+    }
+
+    public int getTab() {
+        return tab;
+    }
+    public void setTab(int tab) {
+        this.tab = tab;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public int getStock() {
+        return this.stock;
+    }
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getInstance_name() {
+        return instance_name;
+    }
+    public void setInstance_name(String instance_name) {
+        this.instance_name = instance_name;
+    }
 
 
+   //Methodes de classe
+    public String toString(){
+        return this.getNomProduits()+": "+tabQuant[this.getTab()];
+    }
 }
